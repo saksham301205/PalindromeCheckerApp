194688faac5f43bdd2d52be7cb3904067c63f1e8
saksham301205 <skmbegad@gmail.com>
2026-02-26 15:18:40 +0530

    UC7: Move palindrome logic to method

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Palindrome Checker Application");
         Scanner sc = new Scanner(System.in);
 
@@ -9,18 +12,8 @@ public class PalindromeCheckerApp {
         do {
             System.out.println("Enter a word:");
             String word = sc.nextLine();
-            word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-            boolean isPalindrome = true;
-
-            for (int i = 0; i < word.length() / 2; i++) {
-                if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            if (isPalindrome) {
+            if (checkPalindrome(word)) {
                 System.out.println(word + " is a Palindrome");
             } else {
                 System.out.println(word + " is not a Palindrome");
@@ -31,6 +24,18 @@ public class PalindromeCheckerApp {
             sc.nextLine();
 
         } while (choice == 'y' || choice == 'Y');
-       sc.close();
+
+        sc.close();
+    }
+
+    public static boolean checkPalindrome(String word) {
+        word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
