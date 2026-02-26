038b8c11021370b370f40c1450de3f23a64b6832
saksham301205 <skmbegad@gmail.com>
2026-02-26 15:13:00 +0530

    UC6: Multiple palindrome checks using loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,24 +4,33 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to Palindrome Checker Application");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a word:");
-        String word = sc.nextLine();
-        word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        char choice;
 
-        boolean isPalindrome = true;
+        do {
+            System.out.println("Enter a word:");
+            String word = sc.nextLine();
+            word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
+            boolean isPalindrome = true;
+
+            for (int i = 0; i < word.length() / 2; i++) {
+                if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
             }
-        }
 
-        if (isPalindrome) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is not a Palindrome");
-        }
-        sc.close();
+            if (isPalindrome) {
+                System.out.println(word + " is a Palindrome");
+            } else {
+                System.out.println(word + " is not a Palindrome");
+            }
+
+            System.out.println("Do you want to continue? (y/n)");
+            choice = sc.next().charAt(0);
+            sc.nextLine();
+
+        } while (choice == 'y' || choice == 'Y');
+       sc.close();
     }
 }
