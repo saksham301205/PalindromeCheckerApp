b7174bf56140c065c75245823616fa748a8b5e84
codeSamurai05032023 <ankursinha2004@gmail.com>
2026-03-06 15:54:51 +0530

    UC7: Deque based optimized palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker Application");
         Scanner sc = new Scanner(System.in);
-
         char choice;
 
         do {
-            System.out.println("Enter a word:");
+
+            System.out.print("Enter a word: ");
             String word = sc.nextLine();
 
-            if (checkPalindrome(word)) {
-                System.out.println(word + " is a Palindrome");
+            if (checkPalindromeUsingDeque(word)) {
+                System.out.println("It is a Palindrome.");
             } else {
-                System.out.println(word + " is not a Palindrome");
+                System.out.println("It is NOT a Palindrome.");
             }
 
-            System.out.println("Do you want to continue? (y/n)");
+            System.out.print("Do you want to continue? (y/n): ");
             choice = sc.next().charAt(0);
             sc.nextLine();
 
@@ -28,14 +27,26 @@ public class PalindromeCheckerApp {
         sc.close();
     }
 
-    public static boolean checkPalindrome(String word) {
+    public static boolean checkPalindromeUsingDeque(String word) {
+
         word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : word.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
                 return false;
             }
         }
+
         return true;
     }
 }
